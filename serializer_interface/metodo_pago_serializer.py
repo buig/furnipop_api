f4efaa8b01d2c5b9aa11049b8fb328dfddb09834
haruueel <93219383+haruueel@users.noreply.github.com>
2023-01-13 17:50:42 +0100

    Ahora devuelve ID

--- serializer_interface/metodo_pago_serializer.py
+++ serializer_interface/metodo_pago_serializer.py
@@ -2,19 +2,13 @@ from rest_framework import serializers
 
 from furnipop_api.models import MetodoPago
 
-class MetodoPagoSerializer(serializers.Serializer):
-    nombre = serializers.CharField(max_length=45)
-    cliente_id = serializers.IntegerField()
+class MetodoPagoSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
 
         return MetodoPago.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
 
-        instance.nombre = validated_data.get('nombre',instance.nombre)
-        instance.cliente_id = validated_data.get('cliente_id',instance.cliente_id)
-
-        instance.save()
-
-        return instance
+    class Meta:
+        model = MetodoPago
+        fields = '__all__'
