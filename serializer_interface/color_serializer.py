f025a1c3f1df2e22a728c89700687a5a555c1fa3
Cesar Frutos X <cesarfrutos_x@yahoo.es>
2023-01-13 16:20:44 +0100

    Colores, departamento y cliente modeleado

--- serializer_interface/color_serializer.py
+++ serializer_interface/color_serializer.py
@@ -2,17 +2,9 @@ from rest_framework import serializers
 
 from furnipop_api.models import Color
 
-class ColorSerializer(serializers.Serializer):
-    nombre = serializers.CharField(max_length=45)
+class ColorSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
 
-    def create(self, validated_data):
-
-        return Color.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-
-        instance.nombre = validated_data.get('nombre',instance.nombre)
-
-        instance.save()
-
-        return instance
+    class Meta:
+        model = Color
+        fields = '__all__'
--- serializer_interface/departamento_serializer.py
+++ serializer_interface/departamento_serializer.py
@@ -4,23 +4,8 @@ from furnipop_api.models import Departamento
 
 class DepartamentoSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(required=False)
-    nombre = serializers.CharField(max_length=45)
-    codigo = serializers.CharField(max_length=45)
-
-    def create(self, validated_data):
-
-        return Departamento.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-
-        instance.nombre = validated_data.get('nombre',instance.nombre)
-        instance.codigo = validated_data.get('codigo',instance.codigo)
-
-        instance.save()
-
-        return instance
 
     class Meta:
         model = Departamento
-        #fields = '__all__'
-        fields = ['id','nombre','codigo']
+        fields = '__all__'
+        #fields = ['id','nombre','codigo']
