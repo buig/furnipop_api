4aa461b4f828313337581a4ee9446e98cb17b298
haruueel <93219383+haruueel@users.noreply.github.com>
2023-01-13 16:01:51 +0100

    Ahora devuelve el ID

--- serializer_interface/camion_serializer.py
+++ serializer_interface/camion_serializer.py
@@ -2,16 +2,12 @@ from rest_framework import serializers
 
 from furnipop_api.models import Camion
 
-class CamionSerializer(serializers.Serializer):
-    matricula = serializers.CharField(max_length=10)
+class CamionSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
 
         return Camion.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-
-        instance.matricula = validated_data.get('matricula',instance.matricula)
-        instance.save()
-
-        return instance
+    class Meta:
+        model = Camion
+        fields = '__all__'
