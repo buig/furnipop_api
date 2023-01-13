2eb9881085b51c5a71deb064b2e11e6d639671d7
haruueel <93219383+haruueel@users.noreply.github.com>
2023-01-13 17:51:30 +0100

    Ahora devuelve ID

--- serializer_interface/paypal_serializer.py
+++ serializer_interface/paypal_serializer.py
@@ -2,19 +2,13 @@ from rest_framework import serializers
 
 from furnipop_api.models import Paypal
 
-class PaypalSerializer(serializers.Serializer):
-    email = serializers.CharField(max_length=45)
-    metodo_pago_id = serializers.IntegerField()
+class PaypalSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
 
         return Paypal.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
 
-        instance.email = validated_data.get('email',instance.email)
-        instance.metodo_pago_id = validated_data.get('metodo_pago_id',instance.metodo_pago_id)
-
-        instance.save()
-
-        return instance
+    class Meta:
+        model = Paypal
+        fields = '__all__'
