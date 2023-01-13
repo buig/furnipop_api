8cac3c3e65931083679550186e58227a51d80076
haruueel <93219383+haruueel@users.noreply.github.com>
2023-01-13 17:51:51 +0100

    Ahora devuelve ID

--- serializer_interface/tarjeta_serializer.py
+++ serializer_interface/tarjeta_serializer.py
@@ -2,19 +2,12 @@ from rest_framework import serializers
 
 from furnipop_api.models import Tarjeta
 
-class TarjetaSerializer(serializers.Serializer):
-    email = serializers.CharField(max_length=45)
-    metodo_pago_id = serializers.IntegerField()
+class TarjetaSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
 
         return Tarjeta.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-
-        instance.email = validated_data.get('email',instance.email)
-        instance.metodo_pago_id = validated_data.get('metodo_pago_id',instance.metodo_pago_id)
-
-        instance.save()
-
-        return instance
+    class Meta:
+        model = Tarjeta
+        fields = '__all__'
