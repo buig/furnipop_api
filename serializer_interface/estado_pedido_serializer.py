5d62cbfde3a64ec5b0468e9b6de1f39758adc47f
haruueel <93219383+haruueel@users.noreply.github.com>
2023-01-13 16:03:30 +0100

    Ahora devuelve ID

--- serializer_interface/estado_pedido_serializer.py
+++ serializer_interface/estado_pedido_serializer.py
@@ -2,16 +2,12 @@ from rest_framework import serializers
 
 from furnipop_api.models import EstadoPedido
 
-class EstadoPedidoSerializer(serializers.Serializer):
-    nombre = serializers.CharField(max_length=10)
+class EstadoPedidoSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
 
         return EstadoPedido.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-
-        instance.nombre = validated_data.get('nombre',instance.nombre)
-        instance.save()
-
-        return instance
+    class Meta:
+        model = EstadoPedido
+        fields = '__all__'
